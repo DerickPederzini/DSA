ba626f2acbe1e27988ed1efaf2b34814ed9084bb
DerickPederzini <derickpps@gmail.com>
2024-01-04 16:01:37 -0300

    first commit

--- GridTravel/Program.cs
+++ GridTravel/Program.cs
@@ -8,8 +8,39 @@ namespace GridTravel
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+            Dictionary<string, long> table = new Dictionary<string, long>();
+
+            Console.WriteLine(grid(18, 18, table));
+            Console.WriteLine(gridNotDynamic(4, 3));
+
+            Console.ReadKey();
+
+        }
+
+        public static long grid(int r, int c, Dictionary<string, long> memo)
+        {
+
+            String key = r + "," + c;
+
+            if (memo.ContainsKey(key)) return memo[key];
+
+            if (r == 1 && c == 1) return 1L;
+            if (r == 0 || c == 0) return 0L;
+
+            memo.Add(key, grid(r - 1, c, memo) + grid(r, c - 1, memo));
+
+            return memo[key];
+        }
+
+        public static int gridNotDynamic(int r, int c)
+        {
+            if (r == 0 || c == 0) return 0;
+            if (r == 1 && c == 1) return 1;
+
+            return gridNotDynamic(r - 1, c) + gridNotDynamic(r, c - 1);
         }
     }
 }
--- fib/Program.cs
+++ fib/Program.cs
@@ -10,6 +10,33 @@ namespace fib
     {
         static void Main(string[] args)
         {
+            Dictionary<int, int> table = new Dictionary<int, int>();
+
+            Console.WriteLine(fib(5, table));
+            Console.WriteLine(fib(12, table));
+            Console.WriteLine(fib(7, table));
+            Console.WriteLine(fib(48, table));
+
+            Console.ReadLine();
+        }
+        public static int fib(int n, Dictionary<int, int> table)
+        {
+
+            //  Using normal recursion
+            //        if (n <= 2) return 1;
+            //        return fib(n - 1) + fib(n - 2);
+
+            //  Using Dynamic Programming
+            //memoization
+            //store duplicate subproblems to use later on
+
+            if (table.ContainsKey(n)) return table[n];
+            if (n <= 2) return 1;
+            table.Add(n, (fib(n - 1, table) + fib(n - 2, table)));
+            return table[n];
+
+
+
         }
     }
 }
