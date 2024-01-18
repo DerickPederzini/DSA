d9f678d4d411dcc6f00a6d9389707a74c4ec4934
DerickPederzini <derickpps@gmail.com>
2024-01-18 16:37:51 -0300

    Commit Leetcode questions

---
+++ 136. SingleNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _136.SingleNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleNumber(new int[] { 1, 2, 2, 5, 5 });
+        }
+        public static int SingleNumber(int[] nums)
+        {
+            if(nums.Length < 2)
+                return nums[0];
+            
+
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!keyValuePairs.ContainsKey(nums[i])) keyValuePairs.Add(nums[i], i);
+                else keyValuePairs.Remove(nums[i]);
+            }
+
+            foreach(var i in keyValuePairs.Keys)
+            {
+                return i;
+            }
+
+            return 0;
+
+
+
+        }
+    }
+}
---
+++ 283. MoveZeroes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _283.MoveZeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveZeroes(new int [] { 0, 1, 0, 3, 12 });
+        }
+
+        public static void MoveZeroes(int[] nums)
+        {
+
+            if (nums.Length == 1)
+            {
+                return;
+            }
+                
+            int j = 0;
+
+            //check for the amount of zeroes in array via their index
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+
+            }
+
+            //add the amount of zeroes to the end of array
+            while (j < nums.Length)
+            {
+                nums[j] = 0;
+                j++;
+            }
+
+        }
+    }
+}
