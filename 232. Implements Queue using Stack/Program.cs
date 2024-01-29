8f540f655aa8be5129accc575e258e4832ca34ac
DerickPederzini <derickpps@gmail.com>
2024-01-29 19:42:07 -0300

    Commit couple problems of Bs and queue

---
+++ 232. Implements Queue using Stack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _232.Implements_Queue_using_Stack
+{
+    internal class MyQueue
+    {
+        private Stack<int> s1;
+        private Stack<int> s2;
+        static void Main(string[] args)
+        {
+            MyQueue obj = new MyQueue();
+            obj.Push(12);
+            obj.Push(15);
+            obj.Pop();
+            obj.Peek();
+            obj.Empty();
+        }
+        public MyQueue()
+        {
+            s1 = new Stack<int>();
+            s2 = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            while (s1.Count > 0)
+            {
+                s2.Push(s1.Pop());
+            }
+            s1.Push(x);
+            while (s2.Count > 0)
+            {
+                s1.Push(s2.Pop());
+            }
+
+        }
+
+        public int Pop()
+        {
+            return s1.Pop();
+        }
+
+        public int Peek()
+        {
+            return s1.Peek();
+        }
+
+        public bool Empty()
+        {
+            return s1.Count == 0;
+        }
+    }
+
+}
+
+
+
+
---
+++ 278. FirstBadVersion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _278.FirstBadVersion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstBadVersion(5));
+        }
+
+        //https://leetcode.com/problems/first-bad-version/
+        public static int FirstBadVersion(int n)
+        {
+
+            if (n < 2) return n;
+
+            int start = 0;
+            int end = n;
+
+            while (end > start)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (!IsBadVersion(mid))
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+            return end;
+
+        }
+
+        public static bool IsBadVersion(int n)
+        {
+            //this should do something, but the problem just gives to me
+            return false;
+        }
+    }
+}
